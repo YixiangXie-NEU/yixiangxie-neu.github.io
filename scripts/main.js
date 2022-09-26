e575b049a67c51feca0d5539c4afc6398a7dd01c
Dylan Tse <dylan@Dylans-MacBook-Air.local>
2022-09-26 09:46:45 -0700

    feat: third version with more js and an Easter egg

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
 const footnote = document.getElementsByClassName("footnote")[0];
 const currentDate = new Date();
-
 footnote.innerHTML += " " + currentDate.getFullYear();
+
+let totalClick = 0;
+const spinningCallback = () => {
+  totalClick++;
+  if (totalClick >= 5) {
+    const profilePicture = document.getElementById("profile-picture");
+    profilePicture.classList.add("rotate");
+    console.log("HAPPY CODING!");
+  }
+};
